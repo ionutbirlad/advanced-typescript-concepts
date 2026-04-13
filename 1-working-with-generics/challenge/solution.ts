874ed7a0df794524c409123e913c68ea898d7352
Ionut Birlad <59876794+ionutbirlad@users.noreply.github.com>
2026-04-13 14:32:58 +0000

    feat: solve challenge number 1

--- 1-working-with-generics/challenge/solution.ts
+++ 1-working-with-generics/challenge/solution.ts
@@ -4,9 +4,11 @@
 // The function must maintain type safety for property keys.
 
 // Complete this function
-function sortArray() {}
+function sortArray<T, K extends keyof T>(arr: T[], key: K): T[] {
+  return arr.sort((a: T, b: T) => (a[key] < b[key] ? -1 : 1));
+}
 
 // Test your sorting function with the following array
 let tasks = [{ id: 2, title: "Write Code" }, { id: 1, title: "Compile" }];
-// let sortedTasks = sortArray(tasks, "id");
-// console.log(sortedTasks);  
+let sortedTasks = sortArray(tasks, "id");
+console.log(sortedTasks);  
