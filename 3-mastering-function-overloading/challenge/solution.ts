f9ae574577893c3de6155e4f000867d4af2a2824
Ionut Birlad <59876794+ionutbirlad@users.noreply.github.com>
2026-04-15 10:41:08 +0000

    chore: add notes about function overloading

--- 3-mastering-function-overloading/challenge/solution.ts
+++ 3-mastering-function-overloading/challenge/solution.ts
@@ -17,27 +17,22 @@ sendMessage("Hello, world!"); // Sends a text message
 sendMessage({ html: "<p>Hello, world!</p>" }); // Sends an HTML message
 
 
+// NOTE
 
+// Queste contano anche se non sembra, provare a togleire per vedere
+function getData(id: number): string;
+function getData(name: string): string;
+function getData(name: string, age: number): string;
 
-
-
-
-
-
-
-
-
-
-
-
-// function sendMessage(content: string | { html: string }): void {
-  //if (typeof content === "string") {
-    //console.log("Sending text message:", content);
-  //} else {
-    //console.log("Sending HTML message:", content.html);
-  //}
-//}
-
-// Usage examples
-//sendMessage("Hello, world!"); // Sends a text message
-//sendMessage({ html: "<p>Hello, world!</p>" }); // Sends an HTML message
+function getData(value: number | string, age?: number): string {
+  if (typeof value === 'number') {
+    return `User ID: ${value}`; 
+  } else if (age !== undefined) {
+    return `User Name: ${value}, Age: ${age}`;
+  } else {
+    return `User Name: ${value}`;
+  }
+}
+  
+  
+console.log(getData(23, 'Zia'));
